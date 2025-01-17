ad068274083251054847f046734256b5eff1bccd
Andrea Mayer <andrea.mayer@uniroma2.it>
2025-01-17 12:19:28 +0000

    enhance netrace tool output with structured JSON formatting
    The recent enhancements to the netrace tool focus on improving the
    output formatting by adopting a structured JSON format. This change aims
    to increase both the readability and usability of the data generated by
    the tool.
    
    The new implementation outputs data as fully formatted JSON objects,
    providing detailed information on CPU usage related to various network
    events.
    Each object includes an "epoch" timestamp and an "events" array
    that details CPU usage for RX (receive) and TX (transmit) activities.
    The "cpu_usage" array shows the percentage of CPU resources used for
    processing packets in softirq context.
    
    Notably, there are no CPU usage entries for the EVENT_NET_TX_SOFTIRQ,
    indicating no CPU resources were spent for this transmit event.
    In contrast, the EVENT_NET_RX_SOFTIRQ records CPU usage, with CPU 2
    using about 12.90% and CPU 3 using approximately 25.07% of their
    capacity for receive packet processing.
    
    Here is an example of the newly structured output, formatted for
    improved readability:
    {
        "epoch": "1737114324383",
        "events": [
            {
                "name": "EVENT_NET_TX_SOFTIRQ",
                "cpu_usage": []
            },
            {
                "name": "EVENT_NET_RX_SOFTIRQ",
                "cpu_usage": [
                    {
                        "2": 12.89
                    },
                    {
                        "3": 25.07
                    }
                ]
            }
        ]
    }
    
    NOTE: netrace formats the JSON into a single line for easy parsing.
    
    The netrace output emits this JSON for each measurement taken at regular
    interval times, capturing various events and their associated CPU usage
    metrics.
    
    Signed-off-by: Andrea Mayer <andrea.mayer@uniroma2.it>

--- examples/c/netrace.c
+++ examples/c/netrace.c
@@ -5,15 +5,20 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
+#include <limits.h>
 #include <sys/resource.h>
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
-#include "netrace.skel.h"
+#include <cjson/cJSON.h>
 
+#include "netrace.skel.h"
 #include "netrace.h"
 
 #define INTERVAL_IN_SEC		1
 #define LOAD_PERC_TH		((double)0.01)
+
+/* do not change BUF_SIZE */
+#define BUF_SIZE		32
 
 static volatile sig_atomic_t stop;
 
@@ -95,6 +100,7 @@ static __u64 gettime(void)
 		fprintf(stderr, "Error with gettimeofday! (%i)\n", res);
 		exit(EXIT_FAILURE);
 	}
+
 	return (__u64) t.tv_sec * NANOSEC_PER_SEC + t.tv_nsec;
 }
 
@@ -168,20 +174,261 @@ static double eval_load_perc(__u64 val_ns, double period)
 	return ((val_ns / (double)NANOSEC_PER_SEC) / period) * 100.0;
 }
 
+static int ts_millis(__u64 *tstamp)
+{
+	struct timespec ts;
+
+	if (clock_gettime(CLOCK_TAI, &ts))
+		return -EINVAL;
+
+	*tstamp = (((__u64)ts.tv_sec) * 1000) + (ts.tv_nsec / 1000000);
+
+	return 0;
+}
+
+static int ts_millis_to_string(char *buf, int len)
+{
+	int rc, size;
+	__u64 ts;
+
+	rc = ts_millis(&ts);
+	if (rc)
+		return rc;
+
+	size = snprintf(buf, len, "%llu", ts);
+	if (size >= len)
+		return -ENOMEM;
+
+	return 0;
+}
+
+static int json_add_epoch_timestamp(cJSON *json)
+{
+	char buf[BUF_SIZE] = { 0, };
+	cJSON *epoch;
+	int rc;
+
+	rc = ts_millis_to_string(buf, BUF_SIZE);
+	if (rc)
+		return rc;
+
+	epoch = cJSON_AddStringToObject(json, "epoch", buf);
+	if (!epoch)
+		return -ENOMEM;
+
+	return 0;
+}
+
+static int __json_create_array(cJSON *parent, const char *key, cJSON **arr)
+{
+	cJSON *arr_elem;
+	cJSON_bool rc;
+
+	arr_elem = cJSON_CreateArray();
+	if (!arr_elem)
+		return -ENOMEM;
+
+	rc = cJSON_AddItemToObject(parent, key, arr_elem);
+	if (!rc)
+		goto cleanup;
+
+	*arr = arr_elem;
+
+	return 0;
+
+cleanup:
+	cJSON_Delete(arr_elem);
+
+	return -EINVAL;
+}
+
+static int json_create_events(cJSON *parent, cJSON **ev)
+{
+	return __json_create_array(parent, "events", ev);
+}
+
+static int json_add_event(cJSON *parent, cJSON **event, enum event_type etype)
+{
+	union {
+		cJSON_bool rc_bool;
+		cJSON *rc_json;
+	} rc;
+	const char *event_name;
+	cJSON *ev;
+
+	/* create the specific event object that will be attached to the
+	 * parent as an array element.
+	 */
+	ev = cJSON_CreateObject();
+	if (!ev)
+		return -ENOMEM;
+
+	event_name = event_to_name(etype);
+	if (!event_name)
+		goto cleanup;
+
+	rc.rc_json = cJSON_AddStringToObject(ev, "name", event_name);
+	if (!rc.rc_json)
+		goto cleanup;
+
+	rc.rc_bool = cJSON_AddItemToArray(parent, ev);
+	if (!rc.rc_bool)
+		goto cleanup;
+
+	*event = ev;
+
+	return 0;
+
+cleanup:
+	cJSON_Delete(ev);
+
+	return -EINVAL;
+}
+
+static int json_create_cpu_usage(cJSON *event, cJSON **cpu_usage)
+{
+	return __json_create_array(event, "cpu_usage", cpu_usage);
+}
+
+static int
+json_add_cpu_usage(cJSON *cpu_usage_array, unsigned int cpuid, double usage)
+{
+	char buf[BUF_SIZE] = { 0, };
+	cJSON *obj, *num, *freeobj;
+	cJSON_bool err;
+	int size;
+
+	obj = cJSON_CreateObject();
+	if (!obj)
+		return -ENOMEM;
+
+	freeobj = obj;
+
+	err = cJSON_AddItemToArray(cpu_usage_array, obj);
+	if (!err)
+		goto cleanup;
+
+	/* ownerhsip obj is moved to cpu_usage_array, so we do not need to
+	 * delete it directly here in case of error.
+	 */
+	num = cJSON_CreateNumber(usage);
+	if (!num)
+		return -ENOMEM;
+
+	freeobj = num;
+
+	if (num->valueint == INT_MAX || num->valueint == INT_MIN)
+		goto cleanup;
+
+	size = snprintf(buf, BUF_SIZE, "%u", cpuid);
+	if (size >= BUF_SIZE)
+		goto cleanup;
+
+	err = cJSON_AddItemToObject(obj, buf, num);
+	if (!err)
+		goto cleanup;
+
+	/* ownership of num is moved to obj */
+	return 0;
+
+cleanup:
+	cJSON_Delete(freeobj);
+
+	return -EINVAL;
+}
+
+static int
+json_create_cpu_usage_by_event(cJSON *events,
+			       enum event_type etype, cJSON **cpu_usage)
+{
+	cJSON *event, *usage;
+	int rc;
+
+	rc = json_add_event(events, &event, etype);
+	if (rc)
+		return rc;
+
+	rc = json_create_cpu_usage(event, &usage);
+	if (rc)
+		return rc;
+
+	*cpu_usage = usage;
+
+	return 0;
+}
+
+static int serialize_json(cJSON *json, char **out)
+{
+	char *str;
+
+	str = cJSON_PrintUnformatted(json);
+	if (!str)
+		return -EINVAL;
+
+	*out = str;
+
+	return 0;
+}
+
+static int create_json_events(cJSON **js, cJSON **evs)
+{
+	cJSON *json, *events;
+	int rc;
+
+	json = cJSON_CreateObject();
+	if (!json)
+		return -ENOMEM;
+
+	/* add timestamp */
+	rc = json_add_epoch_timestamp(json);
+	if (rc)
+		goto cleanup;
+
+	rc = json_create_events(json, &events);
+	if (rc)
+		goto cleanup;
+
+	*js = json;
+	*evs = events;
+
+	return 0;
+
+cleanup:
+	cJSON_Delete(json);
+
+	return rc;
+}
+
+static void destroy_json(cJSON *json)
+{
+	cJSON_Delete(json);
+}
+
 static void stats_print(struct record *prev, struct record *rec)
 {
 	unsigned int nr_cpus = libbpf_num_possible_cpus();
+	cJSON *json = NULL, *events, *cpu_usage;
 	__u64 *pcpu_tt_prev, *pcpu_tt_rec;
 	int event_idx, cpu_idx;
 	double period, load;
 	__s64 tdiff_ns;
+	char *jout;
+	int rc;
 
 	period = eval_period(rec, prev);
 	if (period == 0)
 		return;
 
+	/* create json with timestamp and room for events */
+	rc = create_json_events(&json, &events);
+	if (rc)
+		goto fail;
+
 	for (event_idx = 0; event_idx < EVENT_TYPE_MAX + 1; ++event_idx) {
-		const char *event_name = event_to_name(event_idx);
+		rc = json_create_cpu_usage_by_event(events, event_idx,
+						    &cpu_usage);
+		if (rc)
+			goto fail;
 
 		/* pointer to the pcpu counters for the given event type */
 		pcpu_tt_prev = prev->pcpu_event_total_time[event_idx];
@@ -195,10 +442,38 @@ static void stats_print(struct record *prev, struct record *rec)
 			if (load < LOAD_PERC_TH)
 				continue;
 
-			fprintf(stdout, "event:%s - cpu[%d]:%f\n",
-				event_name, cpu_idx, load);
+			rc = json_add_cpu_usage(cpu_usage, cpu_idx, load);
+			if (rc)
+				goto fail;
 		}
 	}
+
+	rc = serialize_json(json, &jout);
+	if (rc)
+		goto fail;
+
+	/* success, we can print out the json */
+	fprintf(stdout, "%s\n", jout);
+	fflush(stdout);
+
+	free(jout);
+
+	/* release memory by freeding json */
+	destroy_json(json);
+
+	return;
+
+fail:
+	/* works also with json == NULL */
+	destroy_json(json);
+
+	fprintf(stderr, "cannot create json for displaying results\n");
+	fflush(stderr);
+
+	exit(EXIT_FAILURE);
+
+	/* should never be reached */
+	return;
 }
 
 static int stats_poll(int fd, int interval)
@@ -276,8 +551,8 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 
-	printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
-	       "to see output of the BPF programs.\n");
+	fprintf(stderr,
+		"Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` to see output of the BPF programs.\n");
 
 	stats_poll(pcpu_fd, INTERVAL_IN_SEC);
 	/* TODO: return the rc of stats_poll ? */
